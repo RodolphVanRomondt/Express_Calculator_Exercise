f372b12d81529dd6eb6e116adb31874650356a37
Rodolph Van Romondt <mr.rodolph@gmail.com>
2024-03-07 13:31:54 -0800

    median's route

--- app.js
+++ app.js
@@ -47,6 +47,49 @@ app.get("/mean", function (req, res) {
 });
 
 
+/*  MEDIAN's route */
+app.get("/median", function (req, res) {
+
+  if (!req.query.nums) {
+    return res.status(400).json(
+      { response: "nums are required" }
+    );
+  }
+
+  const nums = req.query.nums.split(",");
+  const numsInt = [];
+
+  for (let num of nums) {
+    if (isNaN(num)) {
+      return res.status(400).json(
+        { response: `${num} is not a number` }
+      )
+    }
+
+    numsInt.push(+num);
+  }
+
+  numsInt.sort((a, b) => a - b);
+
+  let idx = numsInt.length / 2;
+  let median;
+  
+  if (numsInt.length % 2 === 0) {
+    median = (numsInt[idx - 1] + numsInt[idx]) / 2;
+  } else {
+    median = numsInt[idx - .5];
+  }
+
+  return res.json({
+    response: {
+      operation: "median",
+      value: median
+    }
+  });
+
+});
+
+
 /** Start server on port 3000 */
 app.listen(3000, function() {
   console.log('Server started on port 3000.');
