e88710bb110e87be42cedbacf05863fa8165d14c
Rodolph Van Romondt <mr.rodolph@gmail.com>
2024-03-07 13:32:52 -0800

    mode's route

--- app.js
+++ app.js
@@ -90,6 +90,52 @@ app.get("/median", function (req, res) {
 });
 
 
+/*  MODE's route */
+app.get("/mode", function (req, res) {
+
+  if (!req.query.nums) {
+    return res.status(400).json(
+      { response: "nums are required" }
+    );
+  }
+
+  const nums = req.query.nums.split(",");
+  const occ = {};
+
+  for (let num of nums) {
+    if (isNaN(num)) {
+      return res.status(400).json(
+        { response: `${num} is not a number` }
+      )
+    }
+
+    if (!occ.hasOwnProperty(num)) {
+      occ[num] = 1;
+      console.log("YES");
+    } else {
+      occ[num] += 1;
+    }
+  }
+
+  const max = Math.max(...Object.values(occ));
+
+  function resp(acc, nextVal) {
+    if (nextVal[1] === max) {
+      acc.push(nextVal[0]);
+    }
+    return acc;
+  }
+
+  return res.json({
+    response: {
+      operation: "mode",
+      value: Object.entries(occ).reduce(resp, [])
+    }
+  });
+
+});
+
+
 /** Start server on port 3000 */
 app.listen(3000, function() {
   console.log('Server started on port 3000.');
