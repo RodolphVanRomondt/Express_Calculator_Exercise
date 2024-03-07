1c74fa3f8f517b396e0633363565f883618410d5
Rodolph Van Romondt <mr.rodolph@gmail.com>
2024-03-07 13:30:59 -0800

    mean's route

--- app.js
+++ app.js
@@ -13,6 +13,40 @@ app.get('/', function(req, res) {
 });
 
 
+/*  MEAN's route */
+app.get("/mean", function (req, res) {
+
+  if (!req.query.nums) {
+    return res.status(400).json(
+      { response: "nums are required" }
+    );
+  }
+
+  const nums = req.query.nums.split(",");
+
+  let total = 0;
+
+  for (let num of nums) {
+
+    if (isNaN(num)) {
+      return res.status(400).json(
+        { response: `${num} is not a number` }
+      )
+    }
+
+    total += parseInt(num);
+  }
+
+  return res.json({
+    response: {
+      operation: "mean",
+      value: total / nums.length
+    }
+  });
+
+});
+
+
 /** Start server on port 3000 */
 app.listen(3000, function() {
   console.log('Server started on port 3000.');
